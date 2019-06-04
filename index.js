09b2397a4e44ae0e298b604ae5060e8d0bcd0187
dnfvnsxn <dnfvnsxn@naver.com>
2019-06-04 23:10:18 +0900

    First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "dnfvnsxn", age = 28, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, yau are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+const name = "dnfvnsxn",
+    age = 28,
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, yau are a ${gender}`);
+};
+
+sayHi(name, age);
+// typescript의 법칙
+// 이 파일이 모듈이 된다는 것 이해할 수 있도록하기 위함
+export {};
