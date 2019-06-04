e902eeb42b7198b25671ca23ecaf46e2bdd101b4
dnfvnsxn <dnfvnsxn@naver.com>
2019-06-05 00:09:25 +0900

    Types in Typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "dnfvnsxn", age = 28, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, yau are a ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ const name = "dnfvnsxn",
     age = 28,
     gender = "male";
 
-const sayHi = (name, age, gender?) => {
-    console.log(`Hello ${name}, you are ${age}, yau are a ${gender}`);
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, you are ${age}, yau are a ${gender}`;
 };
 
-sayHi(name, age);
+console.log(sayHi(name, age, gender));
 // typescript의 법칙
 // 이 파일이 모듈이 된다는 것 이해할 수 있도록하기 위함
 export {};
