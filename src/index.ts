81531c2d8674ef0f773247bfe65281755fcad6b0
YUJuhyun <dnfvnsxn.yu@samsung.com>
2019-06-05 10:52:42 +0900

    interfaces on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-const name = "dnfvnsxn",
-    age = 28,
-    gender = "male";
+interface Human {
+    name: string,
+    age: number,
+    gender: string
+}
 
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, yau are a ${gender}`;
+const person = {
+    name: "dnfvnsxn",
+    age: 28,
+    gender: "male"
+}
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, yau are a ${person.gender}`;
 };
 
-console.log(sayHi(name, age, gender));
+console.log(sayHi(person));
 // typescript의 법칙
 // 이 파일이 모듈이 된다는 것 이해할 수 있도록하기 위함
-export {};
+export { };
